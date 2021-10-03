8a3b188e937b64f9209c5e24aa5ce1283deeec50
windercesarmendes <91811464+windercesarmendes@users.noreply.github.com>
2021-10-02 23:20:24 -0300

    mudança de estrutura do projeto.

---
+++ code/trabalhandoListas.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com Listas");
+const listaDestinos = new Array(
+    `Goiânia`,
+    `São paulo`,
+    `Rio de janeiro`);
+listaDestinos.push(`Iporá`); //adicionando um item na lista
+console.log(listaDestinos);
